cbcdf701b7059acc54cd8cc1d563dd5e4f9de40d
Christopher Sharkey <ev2900@gmail.com>
2023-01-22 15:18:49 -0500

    adjusted python script

--- Metric_Collection/metric_collection.py
+++ Metric_Collection/metric_collection.py
@@ -7,11 +7,23 @@ import pandas
 git_hub_user_name = "ev2900"
 bearer_token = open("token.txt", "r").read()
 
-# Read in historic metric data
+# Read in historic data
 metric_data_views = pandas.read_csv ("Metric_Data/views.csv")
+metric_data_unique_visits = pandas.read_csv ("Metric_Data/unique_visits.csv")
 
 # Get list of all repositories for GitHub user
 get_repos = requests.get("https://api.github.com/users/" + git_hub_user_name + "/repos?per_page=250")
+
+# Add a new row if a row for the repository does not already exists
+for repo in get_repos.json():
+
+	repo_name = repo["name"]
+
+	if not (metric_data_views["Repository_Name"] == repo_name).any():
+		metric_data_views.loc["Repository_Name"] = repo_name
+
+	if not (metric_data_unique_visits["Repository_Name"] == repo_name).any():
+		metric_data_unique_visits.loc["Repository_Name"] = repo_name
 
 for repo in get_repos.json():
 
@@ -26,14 +38,23 @@ for repo in get_repos.json():
 		if timestamp not in metric_data_views.columns:
 			metric_data_views[timestamp] = ""
 
-	# Add view data to data frame
+		if timestamp not in metric_data_unique_visits.columns:
+			metric_data_unique_visits[timestamp] = ""
+
+	# Iterate over traffic data by date
 	for date in get_traffic.json()["views"]:
 			
 		repo_name = repo["name"]
 		timestamp = date["timestamp"]
 		views = date["count"]
+		unique_visits = date["uniques"]
 
+		# Add view data to data frame
 		metric_data_views.loc[metric_data_views.Repository_Name == repo_name,timestamp] = views
+
+		# Add unique vists to data frame
+		metric_data_unique_visits.loc[metric_data_unique_visits.Repository_Name == repo_name,timestamp] = unique_visits
 
 # Write updated data frame to CSV
 metric_data_views.to_csv("Metric_Data/views.csv", index=False)
+metric_data_unique_visits.to_csv("Metric_Data/unique_visits.csv", index=False)
