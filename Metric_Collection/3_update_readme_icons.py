117fa7b3b113bed0bb8a62c2b50074d85cb766be
Christopher Sharkey <ev2900@gmail.com>
2023-12-04 13:54:29 -0500

    Updating history

--- Metric_Collection/3_update_readme_icons.py
+++ Metric_Collection/3_update_readme_icons.py
@@ -101,10 +101,10 @@ for repo in repos:
 	unique_visits = get_number_of_unique_visits(repo)
 
 	if len(views) < 5:
-		views = String.format("%04d", views)
+		views = f'{views:04}'
 
 	if len(unique_visits) < 5:
-		unique_visits = String.format("%04d", unique_visits)
+		unique_visits = f'{unique_visits:04}'
 
 	update_readme_icon("C:\\Users\\ev290\\OneDrive\\Desktop\\GitHub\\" + repo, "views", views)
 	update_readme_icon("C:\\Users\\ev290\\OneDrive\\Desktop\\GitHub\\" + repo, "unique-visits", unique_visits)
