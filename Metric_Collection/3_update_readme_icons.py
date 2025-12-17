5b2f20e8a1dad1d9152ab176d3b8f5d2f034ae8d
Christopher Sharkey <ev2900@gmail.com>
2025-12-17 13:37:23 -0500

    Update 3_update_readme_icons.py

--- Metric_Collection/3_update_readme_icons.py
+++ Metric_Collection/3_update_readme_icons.py
@@ -146,14 +146,14 @@ repos = [
     "Iceberg_Glue_register_table",
     "Iceberg_update_metadata_script",
     "Opentelemetry_Tracing_Python",
-    "PyIceberg_Glue_Data_Catalog",
     "FastAPI_Pydantic_Examples",
     "Iceberg_EMR_rewrite_table_path",
     "GotMilk_TwelveLabs",
     "Snowflake_Iceberg_GDC",
     "Snowflake_Iceberg_Horizon",
     "Snowflake_Iceberg_Polaris",
-    "PyIceberg_Glue_Data_Catalog"
+    "PyIceberg_Glue_Data_Catalog",
+    "DuckDB_Glue_Data_Catalog"
 ]
 
 for repo in repos:
